ff1fa95dde1af77d42b0fc1a47aebe69d3458632
Feargal <Feargaldowney123@gmail.com>
2024-09-26 12:35:42 +0100

    Created component containers, implemented basic styling.

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+// import logo from '../logo.svg';
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.App}>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Jammming</h1>
+    </div>
+  );
+}
+
+export default App;
